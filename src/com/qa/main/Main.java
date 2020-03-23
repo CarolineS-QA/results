0b13a3b477aa4b7bef7fd75200ee52cd8aa69bfe
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-03-23 19:56:57 +0000

    part 1 of results exercise

--- src/com/qa/main/Main.java
+++ src/com/qa/main/Main.java
@@ -3,7 +3,12 @@ package com.qa.main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//creates an instance of class 'results'
+		Results results = new Results();
+		// prints results to console
+		System.out.println(results.examResultsBreakdown());
+		// prints overall percentage to console
+		System.out.println(results.examOverallPercentage());
 
 	}
 
--- src/com/qa/main/Results.java
+++ src/com/qa/main/Results.java
@@ -1,5 +1,21 @@
 package com.qa.main;
 
 public class Results {
+	// creates the 5 variables
+	private int Physics = 150;
+	private int Chemistry = 50;
+	private int Biology = 150;
+	private int TotalMarks = Physics + Chemistry + Biology;
+	private int Percentage = (TotalMarks * 100)/450;
 
+	public String examResultsBreakdown() { //returns each exam result and a total
+		return "Physics mark: " + Physics
+				+ "\nChemistry mark: " + Chemistry //used \n to put each on a new line
+				+ "\nBiology mark: " + Biology
+				+ "\nTotal Marks: " + TotalMarks;
+	}
+	
+	public String examOverallPercentage() { //returns the percentage
+		return "Overall percentage: " + Percentage + "%";
+	}
 }
