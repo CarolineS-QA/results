c9066bf7e0fde710f3b8563e74632f655b66f3b6
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-03-23 20:26:04 +0000

    part 3 of results excercise, fail message shows if they fail an exam >60%

--- src/com/qa/main/Results.java
+++ src/com/qa/main/Results.java
@@ -2,11 +2,15 @@ package com.qa.main;
 
 public class Results {
 	// creates the 5 variables
-	private int Physics = 50;
-	private int Chemistry = 50;
+	private int Physics = 150;
+	private int Chemistry = 89; //never liked chemistry, change it's value and run to see pass/fail in the console
 	private int Biology = 150;
 	private int TotalMarks = Physics + Chemistry + Biology;
 	private int Percentage = (TotalMarks * 100)/450;
+	// adding pass variables for each exams individual %
+	private int PhysicsPercent = (Physics * 100)/150; // should expect a score of 90 to pass
+	private double ChemistryPercent = (Chemistry * 100)/150;
+	private int BiologyPercent = (Biology * 100)/150;
 
 	public String examResultsBreakdown() { //returns each exam result and a total
 		return "Physics mark: " + Physics
@@ -15,13 +19,14 @@ public class Results {
 				+ "\nTotal Marks: " + TotalMarks;
 	}
 	
-	public String examOverallPercentage() { //returns the percentage
-		if (Percentage < 60) { //if % is less than 60
+	public String examOverallPercentage() { //returns the percentage and a pass/fail message
+		//important that >= is used instead of > if you want a mark of 60% to pass. Also && = and.
+		if (Percentage >= 60 && PhysicsPercent >= 60 && ChemistryPercent >= 60 && BiologyPercent >= 60) { //if total % is >= 60, and each exam % is >= 60% (90 marks)
 			return "Overall percentage: " + Percentage + "%"
-				+"\n You failed :("; //fail message on new line
+					+"\n Congrats you passed!"; //pass return message on new line
 		} else {
-			return "Overall percentage: " + Percentage + "%"
-					+"\n Congrats!"; //pass return message on new line
+			return  "Overall percentage: " + Percentage + "%"
+				+"\n You failed overall :("; //fail message on new line
 		}
 	}
 }
