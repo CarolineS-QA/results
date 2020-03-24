26cbf7b9126ee94c12609f2391273890cc444485
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2020-03-24 18:50:35 +0000

    part 4, solution 2, adding a fail counter

--- src/com/qa/main/Results.java
+++ src/com/qa/main/Results.java
@@ -1,49 +1,60 @@
 package com.qa.main;
 
 public class Results {
-	// creates the 5 variables
-	private int Physics = 150;
-	private int Chemistry = 100; // change it's value and run to see pass/fail in the
-									// console
-	private int Biology = 89;
-	private int TotalMarks = Physics + Chemistry + Biology;
-	private int Percentage = (TotalMarks * 100) / 450;
+	// Variables for each exam mark
+	// change the values and run to see pass/fail
+	private int physicsMark = 120;
+	private int chemistryMark = 9;
+	private int biologyMark = 125;
+	// Total and % marks for all 3 exams
+	private int totalMarks = physicsMark + chemistryMark + biologyMark;
+	private int overallPercentage = (totalMarks * 100) / 450;
 	// adding pass variables for each exams individual %
-	private int PhysicsPercent = (Physics * 100) / 150; // should expect a score of 90 to pass
-	private int ChemistryPercent = (Chemistry * 100) / 150;
-	private int BiologyPercent = (Biology * 100) / 150;
+	private int physicsPercent = (physicsMark * 100) / 150; // should expect a score of 90 to pass
+	private int chemistryPercent = (chemistryMark * 100) / 150;
+	private int biologyPercent = (biologyMark * 100) / 150;
 
 	public String examResultsBreakdown() { // returns each exam result and a total
-		return "-All Marks- \nPhysics mark: " + Physics + "\nChemistry mark: " + Chemistry // used \n to put each on a
-																							// new line
-				+ "\nBiology mark: " + Biology + "\nTotal Marks: " + TotalMarks + "\n";
+		return "-All Marks- \nPhysics mark: " + physicsMark + "\nChemistry mark: " + chemistryMark // used \n to put
+																									// each on a
+		// new line
+				+ "\nBiology mark: " + biologyMark + "\nTotal Marks: " + totalMarks + "\n";
 	}
 
 	public String examOverallPercentage() { // returns the percentage and a pass/fail message
 		// important that >= is used instead of > if you want a mark of 60% to pass.
 		// Also && = and.
 		// if total % is >= 60, and each exam % is >= 60% (90 marks)
-		if (Percentage >= 60 && PhysicsPercent >= 60 && ChemistryPercent >= 60 && BiologyPercent >= 60) {
+		if (overallPercentage >= 60 && physicsPercent >= 60 && chemistryPercent >= 60 && biologyPercent >= 60) {
 			// pass return message on new line
-			return "Overall percentage: " + Percentage + "%" + "\nCongrats you passed!";
+			return "Congrats you passed!" + "\nYour overall percentage: " + overallPercentage + "%";
 
 		} else { // will run through and print depending on which exams have failed.
+			// created a variable to track number of failed exams.
+			int failCount = 0;
 			System.out.println("Sorry, you failed the following exams:");
-			// physics fail message, only shows if failed physics
-			if (PhysicsPercent < 60) {
-				System.out.println("You failed Physics." + "\nYour result " + PhysicsPercent + "%");
+			// physics fail message, only shows if failed physics and adds +1 to fail
+			// counter
+			if (physicsPercent < 60) {
+				System.out.println("You failed Physics." + "\nYour result " + physicsPercent + "%");
+				failCount++;
 			}
-			// biology fail message, only show if failed biology
-			if (BiologyPercent < 60) {
-				System.out.println("You failed Biology." + "\nYour result " + BiologyPercent + "%");
+			// biology fail message, only show if failed biology and adds +1 to fail counter
+			if (biologyPercent < 60) {
+				System.out.println("You failed Biology." + "\nYour result " + biologyPercent + "%");
+				failCount++;
 			}
-			// chemistry fail message, only show if failed chemistry
-			if (ChemistryPercent < 60) {
-				System.out.println("You failed Chemistry." + "\nYour result " + ChemistryPercent + "%");
+			// chemistry fail message, only show if failed chemistry and adds +1 to fail
+			// counter
+			if (chemistryPercent < 60) {
+				System.out.println("You failed Chemistry." + "\nYour result " + chemistryPercent + "%");
+				failCount++;
 			}
-			// always returns the overall fail message
-			return "\nYour Overall percentage: " + Percentage + "%";
-			// will try and add a fail counter - see next commit!
+			// always returns the overall fail message - includes fail count.
+			return "\nYou failed " + failCount + " exam(s). Your Overall percentage was: " + overallPercentage + "%";
 		}
 	}
 }
+
+//I have changed the variable names to reflect goodPracticeNamingConvention
+//Extra thing I could do: change exam/exams depending on if failCount > 1
